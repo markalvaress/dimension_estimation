0e4a3df38e2b01e0eef6f7e37719f21e55dcc3dd
markalvaress <mark.alvarez2@gmail.com>
2025-03-09 10:09:23 +0000

    changed sampling method for noisy ball

--- math_helpers.py
+++ math_helpers.py
@@ -19,45 +19,65 @@ def noisy_sphere(d: int, r: float, s: float, m: int, seed: int = None) -> np.nda
         phis = rng.uniform(0, np.pi, m)
         pts = r * np.array([np.sin(phis)*np.cos(thetas), np.sin(phis)*np.sin(thetas), np.cos(phis)]).T
     else:
-        #basic idea: randomly sample points from a D-dimensional NORMAL distribution (since normal distribution is spherically symmetric, whereas uniform would lead to overconcentration in corners), then push them onto the unit ball, then add some noise
-        pts = rng.normal(0, 1, (m, d+1))
-        norms = np.linalg.norm(pts, axis = 1)
-        for i in range(m):
-            pts[i] = r * pts[i] / norms[i]
+        # use the n-dml spherical coordinate system from https://en.wikipedia.org/wiki/N-sphere
+        # if we have d-dml sphere then n = d+1.
+        phis_secondary = rng.uniform(0, np.pi, (m, d-1)) # this is phi_1, ..., phi_{d-1} 
+        phis_main = rng.uniform(0, 2*np.pi, m) # similar to thetas before. This is phi_{n-1} (phi_d) in the wiki article 
+
+        pts = np.zeros((m, d+1))
+
+        # convert the the coordinates in R^d into coordinates on the sphere embedded in R^{d+1}
+        for i in range(d-1):
+            pts[:, i] = r * np.cos(phis_secondary[:, i])
+            for j in range(i):
+                pts[:, i] *= np.sin(phis_secondary[:, j])
+
+        # the last two points are slightly different
+        pts[:, d-1] = r * np.cos(phis_main)
+        pts[:, d] = r * np.sin(phis_main)
+        for j in range(d-1):
+                pts[:, d-1] *= np.sin(phis_secondary[:, j])
+                pts[:, d] *= np.sin(phis_secondary[:, j])
 
     # we use an s/3 here as the SD so that virtually all noise (99.7%) will be within 3*sd = s of 0.
     noise = rng.normal(0, s/3, (m, d+1)) 
-    np.clip(noise, -s, s, out = noise) #Bound the noise to strictly within +/- s.
+    np.clip(noise, -s, s, out = noise) # Bound the noise to strictly within +/- s.
     pts += noise
 
     return pts
 
-def ball_volume(d: int, r: float = 1) -> float:
-    """Calculate the volume of the ball of radius r in d dimensions."""
-    w_d = math.pi**(d/2) / math.gamma(d/2 + 1)
+def ball_volume(D: int, r: float = 1) -> float:
+    """Calculate the volume of the ball of radius r in D dimensions."""
+    w_d = math.pi**(D/2) / math.gamma(D/2 + 1) * r^D
     return w_d
 
-def find_geodesic_distance_sphere(p1: np.ndarray, p2: np.ndarray, r: float) -> float:
-    """Finds the geodesic distance between two points on a sphere in arbitrary dimension of radius r.
+def sphere_surface_area(d: int, r: float = 1) -> float:
+    """Calculate the full surface area of the d-dml sphere (sitting in R^{d+1}) of radius r."""
+    n = d+1 # to match the wiki formulas
+    sa = n*math.pi**(n/2) / math.gamma(1 + n/2) * r^d
+    return sa
 
-    Args:
-        p1 (np.ndarray): 1st point
-        p2 (np.ndarray): 2nd point
-        r (float): Radius of the sphere
+# def find_geodesic_distance_sphere(p1: np.ndarray, p2: np.ndarray, r: float) -> float:
+#     """Finds the geodesic distance between two points on a sphere in arbitrary dimension of radius r.
 
-    Raises:
-        ValueError: If for some reason a negative angle between the two points is calculated (this shouldn't happen)
+#     Args:
+#         p1 (np.ndarray): 1st point
+#         p2 (np.ndarray): 2nd point
+#         r (float): Radius of the sphere
 
-    Returns:
-        float: Geodesic (arc) distance between the two points.
-    """
-    assert len(p1) == len(p2), "Points must live in the same space"
-    assert r > 0, "Radius must be positive"
+#     Raises:
+#         ValueError: If for some reason a negative angle between the two points is calculated (this shouldn't happen)
 
-    d_euclidean = np.linalg.norm(p1-p2, 2)
-    theta = 2*np.arcsin(d/(2*r))
-    if theta < 0:
-        raise ValueError(f"Points {p1=} and {p2=} resulted in a negative angle between them.")
-    d_geod = theta*r
+#     Returns:
+#         float: Geodesic (arc) distance between the two points.
+#     """
+#     assert len(p1) == len(p2), "Points must live in the same space"
+#     assert r > 0, "Radius must be positive"
+
+#     d_euclidean = np.linalg.norm(p1-p2, 2)
+#     theta = 2*np.arcsin(d/(2*r))
+#     if theta < 0:
+#         raise ValueError(f"Points {p1=} and {p2=} resulted in a negative angle between them.")
+#     d_geod = theta*r
 
     return d_geod
